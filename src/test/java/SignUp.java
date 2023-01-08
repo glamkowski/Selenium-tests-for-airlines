8688e8c24724f2ed60518855fc17940ff01e0883
Oskar Glamkowski <phposkar@gmail.com>
2023-01-08 14:15:10 +0100

    added testdata

--- src/test/java/SignUp.java
+++ src/test/java/SignUp.java
@@ -1,3 +1,4 @@
+import com.github.javafaker.Faker;
 import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 
@@ -21,6 +22,15 @@ public class SignUp extends TestBase {
                 .findFirst()
                 .ifPresent(e -> e.click());
 
+        driver.findElement(By.name("firstname")).sendKeys(new Faker().name().firstName());
+        driver.findElement(By.name("lastname")).sendKeys(new Faker().name().lastName());
+        driver.findElement(By.name("phone")).sendKeys(new Faker().phoneNumber().cellPhone());
+        driver.findElement(By.name("email")).sendKeys(new Faker().internet().emailAddress());
+        String password = new Faker().internet().password();
+        driver.findElement(By.name("password")).sendKeys(password);
+        driver.findElement(By.name("confirmpassword")).sendKeys(password);
+
+        driver.findElement(By.xpath("//button[text()=' Sign Up']")).click();
 
 
 
